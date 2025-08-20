fc1055f7419bb6cc8686a5f2f9a95d16fc38a9b4
Enzo <enzoamb123@gmail.com>
2025-08-19 22:05:52 -0300

    avances

--- src/controls.js
+++ src/controls.js
@@ -1,17 +1,34 @@
 const channel = new BroadcastChannel("rosco_rEdKggz6Pyt9eO_");
 
-document.getElementById("play").addEventListener("click", () => {
+const btn_play = document.getElementById("play");
+const btn_pasapalabra = document.getElementById("pasapalabra");
+const btn_correcta = document.getElementById("correcta");
+const btn_incorrecta = document.getElementById("incorrecta");
+
+btn_play.addEventListener("click", () => {
   channel.postMessage({ action: "play" });
+  btn_play.disabled = true;
+  btn_pasapalabra.disabled = false;
+  btn_correcta.disabled = false;
+  btn_incorrecta.disabled = false;
 });
 
-document.getElementById("pasapalabra").addEventListener("click", () => {
+btn_pasapalabra.addEventListener("click", () => {
   channel.postMessage({ action: "pasapalabra" });
+  btn_play.disabled = false;
+  btn_pasapalabra.disabled = true;
+  btn_correcta.disabled = true;
+  btn_incorrecta.disabled = true;
 });
 
-document.getElementById("correcta").addEventListener("click", () => {
+btn_correcta.addEventListener("click", () => {
   channel.postMessage({ action: "correcta" });
 });
 
-document.getElementById("incorrecta").addEventListener("click", () => {
+btn_incorrecta.addEventListener("click", () => {
   channel.postMessage({ action: "incorrecta" });
+  btn_play.disabled = false;
+  btn_pasapalabra.disabled = true;
+  btn_correcta.disabled = true;
+  btn_incorrecta.disabled = true;
 });
